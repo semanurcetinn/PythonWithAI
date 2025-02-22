e2a3291dde97fe47490eb80033111b5c68271364
Sema Nur Çetin <sema.55.cetin@gmail.com>
2025-02-22 15:19:27 +0300

    derste yapılanlar eklendi.

--- homework2.py
+++ homework2.py
@@ -55,7 +55,10 @@ print("**********************")
 for i in range(0,51,10):
     print(i)
 print("**********************")
-
+for i in range(5,10):
+    forVariable  = 5
+    print(i)
+print("**********************")
 for kredi in krediler:
     print(kredi)
 print("**********************")
@@ -99,8 +102,29 @@ def sayHello(name):
     print(f"Merhaba {name}")
 sayHello("Sema")
 
-def calculateAndReturn(price, discount):
+#default parametreler her zaman zorunlu parametrelerden sonra gelmelidir.
+def calculateAndReturn(price, discount=5):
     return price - discount
 
 yeniFiyat = calculateAndReturn(200,50)
 print(yeniFiyat)
+
+def sum(*args): # Sınırsız sayıda parametreli fonksiyon oluşturmak için * koyulur
+    if len(args) <= 0:
+        print("En az 1 sayı göndermek zorundasınız")
+    sonuc = 0
+    for sayi in args:
+        sonuc += sayi
+    return sonuc
+
+print(sum(10,20,30,40,50,60,70))
+print(sum(1))
+
+# **kwargs, fonksiyonu çağırırken anahtar değer ilişkisiyle çağırabiliriz.
+# lambda fonks, anonim tek satır, bir başka fonksiyon içinde kullanıldığında anlam kazanır.
+
+topla = lambda a,b: a+b
+print(topla(1,2))
+
+selamla = lambda isim: print(f"Merhaba {isim}")
+selamla("Sema")
