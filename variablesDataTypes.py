cff40ece4259a6ec0c59dc2065c2d56ad6a24f9b
Sema Nur Çetin <sema.55.cetin@gmail.com>
2025-03-08 23:56:14 +0300

    variables and data types added

---
+++ variablesDataTypes.py
@@ -0,0 +1,14 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+birthDate = int(input("Enter the date of birth:"))
+
+print(f"Merhaba {name}! {age} yaşındasın ve {birthDate} yılında doğmuşsun.")
+
+print("********")
+
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+print(f"Toplam: {number1+number2}")
+print(f"Fark: {number1-number2}")
+print(f"Çarpım: {number1*number2}")
+print(f"Bölüm: {number1/number2}")
